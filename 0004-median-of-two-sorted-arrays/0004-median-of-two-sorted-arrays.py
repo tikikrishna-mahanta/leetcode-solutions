d5ea1b1e57701bee757adb586cc0ea5011bd7f55
Tikikrishna Mahanta <tikikrishnam@gmail.com>
2026-09-26 22:54:30 +0530

    Time: 0 ms (100%), Space: 19.5 MB (79.21%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
             
@@ -7,19 +7,23 @@ class Solution:
         low, high = 0, m
         
         while low <= high:
-            i = (low + high) // 2
-            j = (m + n + 1) // 2 - i
+            partitionX = (low + high) // 2
+            partitionY = (m + n + 1) // 2 - partitionX
             
-            nums1_left = nums1[i - 1] if i > 0 else float('-inf')
-            nums1_right = nums1[i] if i < m else float('inf')
-            nums2_left = nums2[j - 1] if j > 0 else float('-inf')
-            nums2_right = nums2[j] if j < n else float('inf')
+            maxLeftX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+            minRightX = float('inf') if partitionX == m else nums1[partitionX]
             
-            if nums1_left <= nums2_right and nums2_left <= nums1_right:
-                if (m + n) % 2 == 1:
-                    return float(max(nums1_left, nums2_left))
-                return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2.0
-            elif nums1_left > nums2_right:
-                high = i - 1
+            maxLeftY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+            minRightY = float('inf') if partitionY == n else nums2[partitionY]
+            
+            if maxLeftX <= minRightY and maxLeftY <= minRightX:
+                if (m + n) % 2 == 0:
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0
+                else:
+                    return float(max(maxLeftX, maxLeftY))
+            elif maxLeftX > minRightY:
+                high = partitionX - 1
             else:
-                low = i + 1
+                low = partitionX + 1
+                
+        return 0.0
